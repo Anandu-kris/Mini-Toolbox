26453708f72c28519f84830e0fd532a78c21dd8e
Anandu-kris <anandunandu1415@gmail.com>
2026-03-20 18:06:42 +0530

    chore: update dependencies and config

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,18 +2,30 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
-import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/store/store";
+import AppProviders from "@/AppProviders";
+import AppRealtimeBootstrap from "@/realtime/AppRealtimeBootstrap";
 
+import "./index.css";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </QueryClientProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <AppProviders>
+              <AppRealtimeBootstrap />
+              <App />
+            </AppProviders>
+          </QueryClientProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
